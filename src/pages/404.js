bf93d1f6a8d893960e6a94e371614a121ef47994
Celine Souchet <celine.souchet@bonitasoft.com>
2021-06-17 15:12:27 +0200

    Customize 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,51 +1,22 @@
 import * as React from "react"
 import { Link } from "gatsby"
+import Img from "../images/404.png";
 
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-
-// markup
 const NotFoundPage = () => {
   return (
-    <main style={pageStyles}>
+    <main>
       <title>Not found</title>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry{" "}
-        <span role="img" aria-label="Pensive emoji">
-          😔
-        </span>{" "}
-        we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
+      <h1>Page not found</h1>
+      <p>
+        Oops, looks like the page cannot be found...
+        <img
+            alt="404 Image"
+            src={ Img }
+            // width="120"
+            // height="120"
+        />
+        <br/><br/>
+        <Link to="/">Go back to home</Link>
       </p>
     </main>
   )
