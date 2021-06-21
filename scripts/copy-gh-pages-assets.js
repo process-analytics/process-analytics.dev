0a4413f5f93818ec031b61b82b34cceb79f9ce60
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2021-06-21 11:57:06 +0200

    simplify config to have gh-pages configuration files in the built site

--- scripts/copy-gh-pages-assets.js
+++
@@ -1,9 +0,0 @@
-const fse = require('fs-extra');
-
-console.info('Copying gh-pages static assets');
-
-const siteDir = 'public';
-fse.ensureDirSync(`${siteDir}`);
-fse.copySync('.github/pages', `${siteDir}`);
-
-console.info('Copy completed');
