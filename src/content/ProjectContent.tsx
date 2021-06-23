c7e849a86ac3a9636764acfebd84cccb9ee608b7
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2021-06-23 16:36:55 +0200

    Start filling projects

--- src/content/ProjectContent.tsx
+++ src/content/ProjectContent.tsx
@@ -1,13 +1,14 @@
 import { Project} from "../theme/types";
 
 export const projects: Project[] = [{
-    name: '',
-    description: '',
-    homepage: '',
-    repository: '',
-    type: '',
-    publishedDate: '',
+    name: 'bpmn-visualization',
+    description: 'xtc',
+    homepage: 'https://cdn.statically.io/gh/process-analytics/bpmn-visualization-examples/master/examples/index.html',
+    repository: 'https://github.com/process-analytics/bpmn-visualization-js',
+    type: 'TS lib',
+    publishedDate: '2021-06-14',
     logo: {
+        // TODO manage logo
         src: '',
         alt: ' Logo'
     }
