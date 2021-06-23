c125c43b69341e17204174564884790fa3d56e71
Celine Souchet <celine.souchet@bonitasoft.com>
2021-06-23 16:11:28 +0200

    Add fake helmet content

---
+++ src/content/HelmetContent.tsx
@@ -0,0 +1,11 @@
+export const helmet = {
+    name: '',
+    description: '',
+    profile: {
+        favicon16: {src: ''},
+        favicon32: {src: ''},
+        bigIcon: {src: ''},
+        appleIcon: {src: ''},
+    }
+};
+
