a99112542e5986dd30164fcac9c3a5b910655977
Celine Souchet <celine.souchet@bonitasoft.com>
2021-06-23 15:51:49 +0200

    Add missing content

---
+++ src/content/LandingContent.tsx
@@ -0,0 +1,8 @@
+import {Landing} from "../theme/types";
+import {socialLinks} from "./SocialLinkContent";
+
+export const landing: Landing = {
+    name: '',
+    roles: [''],
+    socialLinks: socialLinks
+};
---
+++ src/content/WritingContent.tsx
@@ -0,0 +1,16 @@
+import {MediumAuthor, MediumPost} from "../theme/types";
+
+export const mediumPosts: MediumPost[] = [{
+    title: '',
+    text: '',
+    cover: '',
+    url: '',
+    date: '',
+    time: 0
+}];
+
+export const mediumAuthor: MediumAuthor = {
+    id: '',
+    name: '',
+    username: '',
+};
