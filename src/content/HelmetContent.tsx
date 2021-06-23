8e79839eb96dfbb5421282e5d014736424ef10ef
Celine Souchet <celine.souchet@bonitasoft.com>
2021-06-23 16:40:54 +0200

    Top icon

--- src/content/HelmetContent.tsx
+++ src/content/HelmetContent.tsx
@@ -1,10 +1,12 @@
+import BigIcon from '../../public/icons/icon-96x96.png';
+
 export const helmet = {
     name: '',
     description: '',
     profile: {
         favicon16: {src: ''},
         favicon32: {src: ''},
-        bigIcon: {src: ''},
+        bigIcon: {src: BigIcon},
         appleIcon: {src: ''},
     }
 };
