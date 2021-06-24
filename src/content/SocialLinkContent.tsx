261284a413570f341e3775799cde2ba3b3258777
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2021-06-24 16:56:42 +0200

    Social Icon: simplify text displayed in popup

--- src/content/SocialLinkContent.tsx
+++ src/content/SocialLinkContent.tsx
@@ -2,11 +2,11 @@ import {SocialLink} from "../theme/types";
 
 export const socialLinks: SocialLink[] = [{
     url: 'https://github.com/process-analytics',
-    name: 'Process Analytics Github Organization',
+    name: 'GitHub Organization',
     icon: 'github',
 },
     {
         url: 'https://twitter.com/ProcessAnalyti1',
-        name: 'Process Analytics Twitter',
+        name: 'Twitter',
         icon: 'twitter',
     }];
