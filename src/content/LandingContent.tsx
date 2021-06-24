92e93d5c67192672ae94a7a31b912f926904e3f4
Celine Souchet <celine.souchet@bonitasoft.com>
2021-06-24 10:43:13 +0200

    Merge the old home page & the new home page

--- src/content/LandingContent.tsx
+++ src/content/LandingContent.tsx
@@ -2,7 +2,7 @@ import {Landing} from "../theme/types";
 import {socialLinks} from "./SocialLinkContent";
 
 export const landing: Landing = {
-    name: '',
-    roles: [''],
+    name: 'Process Analytics',
+    roles: ['BPMN', 'Visualization', 'Diagrams', 'UI-Components', 'Analytics'],
     socialLinks: socialLinks
 };
---
+++ src/pages/index.tsx
@@ -0,0 +1,21 @@
+import * as React from "react";
+import Layout from "../theme/components/Layout";
+import Header from "../theme/components/Header";
+import Landing from "../theme/sections/Landing";
+import About from "../theme/sections/About";
+import Projects from "../theme/sections/Projects";
+import Writing from "../theme/sections/Writing";
+import Footer from "../theme/components/Footer";
+
+const HomePage = () => (
+    <Layout>
+        <Header />
+        <Landing />
+        <About />
+        <Projects />
+        <Writing />
+        <Footer />
+    </Layout>
+);
+
+export default HomePage
