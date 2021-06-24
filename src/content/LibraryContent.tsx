8776c4fdb4affbfe56e7aa40404cf6946ab591ff
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2021-06-24 15:43:33 +0200

    Library: update bpmn-visualization description

--- src/content/LibraryContent.tsx
+++ src/content/LibraryContent.tsx
@@ -2,7 +2,7 @@ import { Library } from "../theme/types";
 
 export const libraries: Library[] = [{
     name: 'BPMN Visualization',
-    description: 'To visualize process execution data on BPMN diagrams',
+    description: 'A TypeScript library for visualizing process execution data on BPMN diagrams.',
     example: 'https://cdn.statically.io/gh/process-analytics/bpmn-visualization-examples/master/examples/index.html',
     repository: 'https://github.com/process-analytics/bpmn-visualization-js',
     type: 'TypeScript',
