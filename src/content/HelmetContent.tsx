2631636b1db13a1eef768c32be29fcdbaa6dbbae
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2021-06-24 15:29:38 +0200

    Helmet: add description

--- src/content/HelmetContent.tsx
+++ src/content/HelmetContent.tsx
@@ -2,7 +2,7 @@ import BigIcon from '../../public/icons/icon-96x96.png';
 
 export const helmet = {
     name: 'Process Analytics',
-    description: '',
+    description: 'Process Analytics project website',
     profile: {
         favicon16: {src: ''},
         favicon32: {src: ''},
