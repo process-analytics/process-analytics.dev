5650d32c0a563f45431c48603af362fa49b8bfba
Celine Souchet <celine.souchet@bonitasoft.com>
2021-07-02 14:59:08 +0200

    WIP update the markdown of the About section

--- src/content/AboutContent.tsx
+++ src/content/AboutContent.tsx
@@ -2,7 +2,29 @@ import { About } from '../theme/types';
 import Logo from '../images/logo.svg';
 
 export const about: About = {
-  markdown: `Set of open source resources that developers can use to build BPMN-based Process Analytics software to visualize Process-Execution-related data.`,
+  markdown: `WHY?  
+Need to analyze and optimize your business processes? 
+Then you need an overview of the execution data: process, data, actors (human and robots) and systems (forms, apps, 3rd party software).
+
+WHAT?  
+The Process Analytics project supports the creation of tools that can be used to:
+- give an overview of the execution data
+- help to make the right choices for process understanding and optimization
+
+You can create and build applications to visualize:
+- monitoring an individual process instance execution
+- monitoring of all process instances for a selected process
+- prediction of a process instance execution 
+- other process data 
+- drill down through process execution flow
+- related resources data
+- and more…
+
+HOW?  
+The Process Analytics project provides a set of libraries that can be integrated into automation tools, process mining, Business Intelligence tools, RPA, services orchestration, and others.
+
+These libraries will make it easy to render processes, human tasks, automatic and robotic (RPA) talks, and system-related execution data. 
+`,
   logo: {
     src: Logo,
     alt: 'Process Analytics Logo',
