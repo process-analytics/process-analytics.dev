a9287e19207c9aba813eb9b8761cf113a17906de
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2021-07-02 15:31:27 +0200

    More content + alternate image on right and left

--- src/content/AboutContent.tsx
+++ src/content/AboutContent.tsx
@@ -1,13 +1,12 @@
 import { About } from '../theme/types';
 import Logo from '../images/logo.svg';
 
-export const about: About = {
-  markdown: `Set of open source resources that developers can use to build BPMN-based Process Analytics software to visualize Process-Execution-related data.
-Hello 👋 I'm Mate a gatsby starter which is focus on simplicity and extensibility. It's build with Gatsby(duh), [Rebass](https://jxnblk.com/rebass/) (styled-component system) and [Contentful](https://www.contentful.com/).
+const aboutPart1: About = {
+  markdown: `Hello 👋 I'm Mate a gatsby starter which is focus on simplicity and extensibility. It's build with Gatsby(duh), [Rebass](https://jxnblk.com/rebass/) (styled-component system) and [Contentful](https://www.contentful.com/).
 The starter will give you 4 sections (it's really easy to add more if you want 😃):
 * [Landing:](#home) Displays a nice greeting with your name, also your roles (what you  are) and all your social links.
 * [About:](#about) Show the about section where you can write about who you are, what you like to do, etc. Also you can add a photo next to it!
-* [Project:](#projects) Displays a card for all your project that you've made and also the posibility to link with github or and external link.
+* [Project:](#projects) Displays a card for all your project that you've made and also the possibility to link with github or and external link.
 * [Writing:](#writing) Take the information about your medium user and show up to 6 stories as cards with the link to Medium.
   `,
   image: {
@@ -15,3 +14,21 @@ The starter will give you 4 sections (it's really easy to add more if you want �
     alt: 'Process Analytics Logo',
   },
 };
+
+const aboutPart2: About = {
+  markdown: ` Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tristique nec tellus et vulputate. Aliquam a lacinia libero, non facilisis elit. Vivamus tempor gravida diam nec efficitur. Aliquam accumsan orci id mi tincidunt, sed fermentum turpis tristique. Morbi vitae faucibus ante. Quisque enim turpis, faucibus sed ante in, feugiat convallis lorem. Pellentesque tincidunt at libero luctus iaculis. Mauris vel volutpat nulla, in aliquet mi.`,
+  image: {
+    src: Logo,
+    alt: 'Process Analytics Logo',
+  },
+};
+
+const aboutPart3: About = {
+  markdown: `Maecenas pharetra maximus nisl a dignissim. Aliquam malesuada nisl nisl, quis fermentum orci malesuada a. Donec a ex sed tortor auctor interdum quis quis justo. Quisque convallis pretium eros non vulputate. Curabitur nisi eros, semper at varius et, blandit ut risus. Sed accumsan tellus tortor, nec lobortis dolor posuere hendrerit. Donec et dui semper, ultricies turpis eu, egestas magna. Ut lacus purus, faucibus eget interdum ut, dignissim ac justo. Cras fringilla pharetra enim, id porta justo accumsan non. Suspendisse nec ligula at magna malesuada fermentum quis nec turpis. Nam viverra, tellus sit amet dictum fringilla, sem lectus varius nulla, id viverra urna nisl quis neque.`,
+  image: {
+    src: Logo,
+    alt: 'Process Analytics Logo',
+  },
+};
+
+export const abouts = [aboutPart1, aboutPart2, aboutPart3];
--- src/theme/sections/About.tsx
+++ src/theme/sections/About.tsx
@@ -6,18 +6,27 @@ import Section from '../components/Section';
 import Triangle from '../components/Triangle';
 import markdownRenderer from '../components/MarkdownRenderer';
 import { SECTION } from '../utils/constants';
-import { about } from '../../content/AboutContent';
+import { abouts } from '../../content/AboutContent';
 
 const About = (): JSX.Element => {
-  const { markdown, image } = about;
+  // TODO generalize the abouts lookup
+  // alternate image on right or left depending on the loop index
+  // we may create a component to avoid duplication
+  const about1 = abouts[0];
+  const about2 = abouts[1];
+  const about3 = abouts[2];
 
   return (
     <Section.Container id={SECTION.about} Background={Background}>
       <Section.Header name={SECTION.about} />
+
       <Flex justifyContent="center" alignItems="center" flexWrap="wrap">
         <Box width={[1, 1, 4 / 6]} px={[1, 2, 4]} mt={2}>
           <Fade direction="down" triggerOnce>
-            <ReactMarkdown source={markdown} renderers={markdownRenderer} />
+            <ReactMarkdown
+              source={about1.markdown}
+              renderers={markdownRenderer}
+            />
           </Fade>
         </Box>
 
@@ -30,7 +39,56 @@ const About = (): JSX.Element => {
               width={[2 / 6, 2 / 6, 1]}
               mt={[4, 4, 0]}
               ml={[0, 0, 1]}
-              {...image}
+              {...about1.image}
+            />
+          </Fade>
+        </Box>
+      </Flex>
+
+      <Flex justifyContent="center" alignItems="center" flexWrap="wrap">
+        <Box
+          width={[1, 1, 2 / 6]}
+          style={{ maxWidth: '300px', margin: 'auto' }}
+        >
+          <Fade direction="right" triggerOnce style={{ textAlign: 'center' }}>
+            <Image
+              width={[2 / 6, 2 / 6, 1]}
+              mt={[4, 4, 0]}
+              ml={[0, 0, 1]}
+              {...about2.image}
+            />
+          </Fade>
+        </Box>
+        <Box width={[1, 1, 4 / 6]} px={[1, 2, 4]} mt={2}>
+          <Fade direction="down" triggerOnce>
+            <ReactMarkdown
+              source={about2.markdown}
+              renderers={markdownRenderer}
+            />
+          </Fade>
+        </Box>
+      </Flex>
+
+      <Flex justifyContent="center" alignItems="center" flexWrap="wrap">
+        <Box width={[1, 1, 4 / 6]} px={[1, 2, 4]} mt={2}>
+          <Fade direction="down" triggerOnce>
+            <ReactMarkdown
+              source={about3.markdown}
+              renderers={markdownRenderer}
+            />
+          </Fade>
+        </Box>
+
+        <Box
+          width={[1, 1, 2 / 6]}
+          style={{ maxWidth: '300px', margin: 'auto' }}
+        >
+          <Fade direction="right" triggerOnce style={{ textAlign: 'center' }}>
+            <Image
+              width={[2 / 6, 2 / 6, 1]}
+              mt={[4, 4, 0]}
+              ml={[0, 0, 1]}
+              {...about3.image}
             />
           </Fade>
         </Box>
