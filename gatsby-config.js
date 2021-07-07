3dd7d4ab42eb270dc3ea0f116d96cd268fbfd791
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2021-07-07 16:40:53 +0200

    Lint gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,25 +1,25 @@
 const colors = require('./src/theme/colors.json');
 
-module.exports =  {
-    siteMetadata: {
-      title: "Process Analytics",
-      siteUrl: `https://process-analytics.dev`,
-    },
-    plugins: [
-      "gatsby-plugin-sitemap",
-      {
-        resolve: "gatsby-plugin-manifest",
-        options: {
-          name: 'Process Analytics',
-          start_url: '/',
-          background_color: colors.background,
-          theme_color: colors.primary,
-          display: 'minimal-ui',
-          icon: "src/images/logo.svg",
-        },
+module.exports = {
+  siteMetadata: {
+    title: 'Process Analytics',
+    siteUrl: `https://process-analytics.dev`,
+  },
+  plugins: [
+    'gatsby-plugin-sitemap',
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'Process Analytics',
+        start_url: '/',
+        background_color: colors.background,
+        theme_color: colors.primary,
+        display: 'minimal-ui',
+        icon: 'src/images/logo.svg',
       },
-      'gatsby-plugin-react-helmet',
-      'gatsby-plugin-typescript',
-      'gatsby-plugin-styled-components',
-    ],
+    },
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-typescript',
+    'gatsby-plugin-styled-components',
+  ],
 };
