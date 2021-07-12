e9d0360dcf18b0a0d20f046c5c44ce44382c9230
Souchet Céline <4921914+csouchet@users.noreply.github.com>
2021-07-12 16:30:35 +0200

    [FEAT] Setup analytics (visits and views) (#56)

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,25 +1,45 @@
 const colors = require('./src/theme/colors.json');
 
-module.exports =  {
-    siteMetadata: {
-      title: "Process Analytics",
-      siteUrl: `https://process-analytics.dev`,
+module.exports = {
+  siteMetadata: {
+    title: 'Process Analytics',
+    siteUrl: `https://process-analytics.dev`,
+  },
+  plugins: [
+    'gatsby-plugin-sitemap',
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'Process Analytics',
+        start_url: '/',
+        background_color: colors.background,
+        theme_color: colors.primary,
+        display: 'minimal-ui',
+        icon: 'src/images/logo.svg',
+      },
     },
-    plugins: [
-      "gatsby-plugin-sitemap",
-      {
-        resolve: "gatsby-plugin-manifest",
-        options: {
-          name: 'Process Analytics',
-          start_url: '/',
-          background_color: colors.background,
-          theme_color: colors.primary,
-          display: 'minimal-ui',
-          icon: "src/images/logo.svg",
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-typescript',
+    'gatsby-plugin-styled-components',
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        trackingIds: [
+          'G-D2D0FR22RN', // Google Analytics
+        ],
+        // This object gets passed directly to the gtag config command
+        gtagConfig: {
+          // see https://www.gatsbyjs.com/plugins/gatsby-plugin-google-gtag/#the-gtagconfiganonymize_ip-option
+          anonymize_ip: true,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+          // Google Global Site Tag will not be loaded at all for visitors that have “Do Not Track” enabled.
+          respectDNT: true,
         },
       },
-      'gatsby-plugin-react-helmet',
-      'gatsby-plugin-typescript',
-      'gatsby-plugin-styled-components',
-    ],
+    },
+  ],
 };
