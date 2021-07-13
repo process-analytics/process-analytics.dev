a3aaf2ab3f90653afdc5dd3fa2f73e3e50578b51
Celine Souchet <celine.souchet@bonitasoft.com>
2021-07-13 11:45:01 +0200

    Remove the title of the sub-sections

--- src/content/AboutContent.tsx
+++ src/content/AboutContent.tsx
@@ -3,8 +3,7 @@ import Logo from '../images/logo.svg';
 
 export const about: About = [
   {
-    markdown: `WHY?  
-Need to analyze and optimize your business processes? 
+    markdown: `Need to analyze and optimize your business processes? 
 Then you need an overview of the execution data: process, data, actors (human and robots) and systems (forms, apps, 3rd party software).`,
     image: {
       src: Logo,
@@ -12,8 +11,7 @@ Then you need an overview of the execution data: process, data, actors (human an
     },
   },
   {
-    markdown: `WHAT?  
-The Process Analytics project supports the creation of tools that can be used to:
+    markdown: `The Process Analytics project supports the creation of tools that can be used to:
 - give an overview of the execution data
 - help to make the right choices for process understanding and optimization`,
     image: {
@@ -37,8 +35,7 @@ The Process Analytics project supports the creation of tools that can be used to
     },
   },
   {
-    markdown: `HOW?  
-The Process Analytics project provides a set of libraries that can be integrated into automation tools, process mining, Business Intelligence tools, RPA, services orchestration, and others.
+    markdown: `The Process Analytics project provides a set of libraries that can be integrated into automation tools, process mining, Business Intelligence tools, RPA, services orchestration, and others.
 
 These libraries will make it easy to render processes, human tasks, automatic and robotic (RPA) talks, and system-related execution data.`,
     image: {
