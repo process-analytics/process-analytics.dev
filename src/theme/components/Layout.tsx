532b2bd37d4240948e7a77be195349e73e802614
Celine Souchet <celine.souchet@bonitasoft.com>
2021-08-05 16:16:01 +0200

    Configure cookie consent bar & disable Google analytics if decline (only after page refresh)

--- src/theme/components/Layout.tsx
+++ src/theme/components/Layout.tsx
@@ -5,6 +5,7 @@ import { loadIcons } from '../utils/icons';
 import { theme } from '../theme';
 import Helmet from './Helmet';
 import 'tippy.js/dist/tippy.css';
+import CookieConsent from 'react-cookie-consent';
 
 declare module 'styled-components' {
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
@@ -47,6 +48,23 @@ const Layout = ({ children, title }: Props): JSX.Element => (
       <GlobalStyle />
       <Helmet title={title} />
       {children}
+      <CookieConsent
+        location="bottom"
+        cookieName={'pa_ga-disable-G-D2D0FR22RN'}
+        enableDeclineButton={true}
+        flipButtons={true}
+        cookieValue={false}
+        declineCookieValue={true}
+        /*        onDecline={() => {
+          window['pa_ga-disable-G-D2D0FR22RN'] = true;
+          document.cookie =
+            'pa_ga-disable-G-D2D0FR22RN=true; expires=Thu, 31 Dec 2099 23:59:59 UTC;path=/';
+          window.disableStr = 1;
+        }}*/
+      >
+        This website uses cookies to enhance the user experience.
+      </CookieConsent>
+      {/*      <a href="javascript:gaOptout();">Deactivate Google Tracking</a>*/}
     </ThemeProvider>
   </main>
 );
