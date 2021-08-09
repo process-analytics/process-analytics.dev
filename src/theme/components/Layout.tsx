1ae4092f53054ec35afbb9b07900f18d4a62ac28
Souchet Céline <4921914+csouchet@users.noreply.github.com>
2021-08-09 12:11:20 +0200

    [FEAT] Let user consent or disable Google Analytics (#101)
    * Add react-cookie-consent
    * Configure cookie consent bar & disable Google analytics if decline, even without page refresh, by calling a javascript function ('gaOptout') of gatsby-plugin-google-gtag
    * Customize the style of the consent banner
    * Add page obscuring overlay when display the consent banner
    
    Co-authored-by: Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>

--- src/theme/components/Layout.tsx
+++ src/theme/components/Layout.tsx
@@ -5,11 +5,17 @@ import { loadIcons } from '../utils/icons';
 import { theme } from '../theme';
 import Helmet from './Helmet';
 import 'tippy.js/dist/tippy.css';
+import CookieConsent, { Cookies } from 'react-cookie-consent';
 
 declare module 'styled-components' {
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
   export type DefaultTheme = Theme;
+}
+
+declare global {
+  // This function is made available by the plugin gatsby-plugin-google-gtag at runtime
+  function gaOptout(): void;
 }
 
 loadIcons();
@@ -47,6 +53,38 @@ const Layout = ({ children, title }: Props): JSX.Element => (
       <GlobalStyle />
       <Helmet title={title} />
       {children}
+      <CookieConsent
+        location="bottom"
+        cookieName={'ga-disable-G-D2D0FR22RN'}
+        enableDeclineButton={true}
+        flipButtons={true}
+        style={{
+          opacity: '90%',
+        }}
+        contentStyle={{
+          color: theme.colors.background,
+        }}
+        cookieValue={false}
+        buttonStyle={{
+          background: '#90EE90',
+          color: theme.colors.primary,
+        }}
+        declineCookieValue={true}
+        declineButtonStyle={{
+          background: theme.colors.secondary,
+          color: theme.colors.background,
+        }}
+        onDecline={() => {
+          gaOptout();
+
+          // Clean the unnecessary cookies
+          Cookies.remove('_ga', { path: '/' });
+          Cookies.remove('_ga_D2D0FR22RN', { path: '/' });
+        }}
+        overlay={true}
+      >
+        This website uses cookies to monitor the audience.
+      </CookieConsent>
     </ThemeProvider>
   </main>
 );
