6b61de44ed729360c63c8246a0832bfcf7ae6d4a
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2021-08-17 15:42:22 +0200

    fix lint error

--- src/theme/components/Helmet.tsx
+++ src/theme/components/Helmet.tsx
@@ -39,7 +39,10 @@ const Helmet = ({ theme, title, keywords }: Props): JSX.Element => {
       <meta name="twitter:description" content={description} />
       {/*no need for twitter:image:src here as the svg image is inline in base64 */}
       <meta name="twitter:image" content={`${profile.logo.src}`} />
-      <meta name="google-site-verification" content="qp3t994P7OXLorDDzsmT0N_-KaxDwvjuNuJbxkbjKrM" />
+      <meta
+        name="google-site-verification"
+        content="qp3t994P7OXLorDDzsmT0N_-KaxDwvjuNuJbxkbjKrM"
+      />
     </ReactHelmet>
   );
 };
