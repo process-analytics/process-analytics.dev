1e317d5cd365af48a81daa950ce770f01e186dba
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2021-08-17 12:49:25 +0200

    rename import

--- src/content/HelmetContent.tsx
+++ src/content/HelmetContent.tsx
@@ -1,4 +1,4 @@
-import BigIcon from '../images/logo_white.svg';
+import SvgLogoWhite from '../images/logo_white.svg';
 import { landing } from './LandingContent';
 
 export const helmet = {
@@ -6,7 +6,7 @@ export const helmet = {
   profile: {
     // favicon16: { src: '' },
     // favicon32: { src: '' },
-    bigIcon: { src: BigIcon },
+    bigIcon: { src: SvgLogoWhite },
     // appleIcon: { src: '' },
   },
 };
