901593d9bdd733d53f0c7f9a6f75b0379fae144c
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2021-08-17 15:46:41 +0200

    remove TODO

--- src/content/HelmetContent.tsx
+++ src/content/HelmetContent.tsx
@@ -2,7 +2,7 @@ import SvgLogoWhite from '../images/logo_white.svg';
 import { landing } from './LandingContent';
 
 export const helmet = {
-  description: landing.presentation, // TODO ensure this is not too long (160 chars) see https://www.gatsbyjs.com/tutorial/seo-and-social-sharing-cards-tutorial/#querying-with-graphql
+  description: landing.presentation, // This should not be too long (160 chars)  see https://www.gatsbyjs.com/tutorial/seo-and-social-sharing-cards-tutorial/#querying-with-graphql
   profile: {
     logo: { src: SvgLogoWhite },
   },
--- src/content/LandingContent.tsx
+++ src/content/LandingContent.tsx
@@ -3,6 +3,7 @@ import { socialLinks } from './SocialLinkContent';
 
 export const landing: Landing = {
   title: 'Process Analytics',
+  // WARN: if it is longer than 160 characters, please do ellipsis in HelmetContent
   presentation:
     'Rapidly display meaningful Process Analytics components in your web pages using BPMN 2.0 notation and Open Source libraries.',
   socialLinks: socialLinks,
