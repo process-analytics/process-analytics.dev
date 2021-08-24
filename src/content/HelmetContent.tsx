bea97dd308ed16a28ff3cbf2dae23fb9e664458a
Celine Souchet <celine.souchet@bonitasoft.com>
2021-08-24 17:52:14 +0200

    change the name of the OG card

--- src/content/HelmetContent.tsx
+++ src/content/HelmetContent.tsx
@@ -3,7 +3,7 @@ import { landing } from './LandingContent';
 export const helmet = {
   description: landing.presentation, // This should not be too long (160 chars)  see https://www.gatsbyjs.com/tutorial/seo-and-social-sharing-cards-tutorial/#querying-with-graphql
   logo: {
-    src: '/image/twitter_card.webp',
+    src: '/image/og_card.webp',
   },
   twitter: {
     username: '@ProcessAnalyti1',
