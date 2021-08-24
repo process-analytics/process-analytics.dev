1cd0082c27760585a28af26d07c917d96370f8be
Celine Souchet <celine.souchet@bonitasoft.com>
2021-08-24 11:51:18 +0200

    Fix logo color on helmet

--- src/content/HelmetContent.tsx
+++ src/content/HelmetContent.tsx
@@ -1,9 +1,9 @@
-import SvgLogoWhite from '../images/logo_white.svg';
+import SvgLogo from '../images/logo.svg';
 import { landing } from './LandingContent';
 
 export const helmet = {
   description: landing.presentation, // This should not be too long (160 chars)  see https://www.gatsbyjs.com/tutorial/seo-and-social-sharing-cards-tutorial/#querying-with-graphql
   profile: {
-    logo: { src: SvgLogoWhite },
+    logo: { src: SvgLogo },
   },
 };
