4921bf91ad5292d35c808e604a388083a215ce47
Souchet Céline <4921914+csouchet@users.noreply.github.com>
2021-08-31 16:34:53 +0200

    [DOC] Add copyright header on all code files (#142)
    * Install & Configure eslint-plugin-notice
    * Add header in all files

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,7 @@
  * limitations under the License.
  */
 module.exports = {
+  plugins: ['notice'],
   parser: '@typescript-eslint/parser', // Specifies the ESLint parser
   extends: [
     'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
@@ -23,6 +24,13 @@ module.exports = {
     sourceType: 'module', // Allows for the use of imports
   },
   rules: {
+    'notice/notice': [
+      'error',
+      {
+        templateFile: 'config/license-header.js',
+        onNonMatchingHeader: 'replace',
+      },
+    ],
     'no-console': ['error', { allow: ['warn', 'error'] }],
   },
   overrides: [
