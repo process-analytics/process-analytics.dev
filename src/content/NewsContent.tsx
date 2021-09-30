3491e97dcdfa1f68e3f8fbfb07cfe068bf1603d9
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2021-09-30 18:56:55 +0200

    [FEAT] Add hacktoberfest 2021 news

--- src/content/NewsContent.tsx
+++ src/content/NewsContent.tsx
@@ -17,6 +17,14 @@ import { PostDescription } from '../theme/types';
 
 const news: PostDescription[] = [
   {
+    title: 'Hacktoberfest 2021 with Process Analytics',
+    text: 'The Process Analytics team is participating in Hacktoberfest 2021, as we did for Hacktoberfest 2020.',
+    cover: 'https://miro.medium.com/max/350/0*nuqEJFMVU_qIE2ZO',
+    url: 'https://medium.com/@process-analytics/hacktoberfest-2021-with-process-analytics-44eecc238ead',
+    date: 'September 2021',
+    time: 2,
+  },
+  {
     title: 'August 2021 Newsletter',
     text: 'Learn about the new Process Analytics website and logo, the new R library and improvements in the BPMN Visualization Typescript library.',
     cover: 'https://miro.medium.com/max/310/0*Yymlf2vjfAIINqc2',
