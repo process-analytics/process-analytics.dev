1dc83d3e404b4db2cc05293b9c56db5c1972145a
Souchet Céline <celine.souchet@bonitasoft.com>
2021-11-17 16:42:58 +0100

    7 - More space between the More button & the card container

--- src/theme/components/Post.tsx
+++ src/theme/components/Post.tsx
@@ -93,7 +93,7 @@ export const PostContainer = ({
         </DownFade>
       </CardContainer>
       {pageId && (
-        <Flex justifyContent="center" mt="3" fontSize={[2, 3]}>
+        <Flex justifyContent="center" mt="4" fontSize={[2, 3]}>
           <DownFade>
             <Button variant="more">
               <RebassLink
