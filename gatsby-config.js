82175bdc09b5b77d8ad74ff1fd97f4eea845ca4d
Souchet Céline <celine.souchet@bonitasoft.com>
2021-11-24 18:22:05 +0100

    Configure Mailchimp & add Type definition

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2021 Bonitasoft S.A.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 const colors = require('./src/theme/colors.json');
 
 // Only the variables prefixed by 'GATSBY_' can be available in browser code
@@ -59,6 +74,14 @@ module.exports = {
       resolve: `gatsby-plugin-mdx`,
       options: {
         extensions: [`.mdx`, `.md`],
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-mailchimp',
+      options: {
+        endpoint:
+          'https://gmail.us20.list-manage.com/subscribe/post?u=98bed213911ed14e04f519b82&amp;id=9c27a4ee73',
+        timeout: 3500, //the amount of time, in milliseconds, that you want to allow mailchimp to respond to your request before timing out. defaults to 3500
       },
     },
   ],
