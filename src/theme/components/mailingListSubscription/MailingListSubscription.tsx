8b5463e4a9e11c746dd3e9249d119e88b86dd40e
Souchet Céline <celine.souchet@bonitasoft.com>
2021-12-03 17:15:53 +0100

    Fix lint

--- src/theme/components/mailingListSubscription/MailingListSubscription.tsx
+++ src/theme/components/mailingListSubscription/MailingListSubscription.tsx
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import { faEnvelope, faWindowClose } from '@fortawesome/free-solid-svg-icons';
 import { ContainerContent } from './ContainerContent';
