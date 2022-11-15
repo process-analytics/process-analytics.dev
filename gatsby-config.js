2eacc96f37b727f8a595665ff217e7c5f3505b62
Souchet Céline <celine.souchet@bonitasoft.com>
2022-11-15 15:34:15 +0100

    Add mandatory gatsby-source-filesystem dependency for gatsby-plugin-mdx

--- gatsby-config.js
+++ gatsby-config.js
@@ -78,6 +78,13 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages`,
+      },
+    },
+    {
       resolve: 'gatsby-plugin-mailchimp',
       options: {
         endpoint:
