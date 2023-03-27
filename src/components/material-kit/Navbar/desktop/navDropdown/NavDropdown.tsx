6084e20ede3b1db5f65a6f8c997af3acb152464f
Souchet Céline <celine.souchet@bonitasoft.com>
2023-03-27 11:29:36 +0200

    Extract ColumnDropdownMenus for desktop

--- src/components/material-kit/Navbar/desktop/navDropdown/NavDropdown.tsx
+++ src/components/material-kit/Navbar/desktop/navDropdown/NavDropdown.tsx
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 /**
  =========================================================
  * Material Kit 2 React - v2.0.0
