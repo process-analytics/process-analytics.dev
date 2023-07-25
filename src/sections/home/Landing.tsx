0af7b2ae9217fcc99240b0a14c15bca24cd7cdf6
Souchet Céline <celine.souchet@bonitasoft.com>
2023-07-25 14:59:15 +0200

    fix bad merge

--- src/sections/home/Landing.tsx
+++ src/sections/home/Landing.tsx
@@ -19,7 +19,7 @@ import { Heading, Flex, Box, Text } from 'rebass/styled-components';
 
 import { Fade } from 'react-awesome-reveal';
 
-import { Section, SocialLink, SocialLinkProps } from '../../theme/components';
+import { Section, SocialLink, SocialLinkContent } from '../../theme/components';
 import { SECTION } from '../../theme/utils/constants';
 
 import { landing } from '../../content/LandingContent';
