56ade09f8e0c7904f76f33896c3786cce3fd69ad
Souchet Céline <4921914+csouchet@users.noreply.github.com>
2023-08-08 14:01:03 +0200

    feat(button): use MaterialUI in the Button component (#1103)

--- src/components/PostContainer.tsx
+++ src/components/PostContainer.tsx
@@ -13,16 +13,16 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import Container from '@mui/material/Container';
-import Grid from '@mui/material/Grid';
+
 import React from 'react';
 
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+
 import { Flex } from 'rebass/styled-components';
-import { PostCard, MKBox } from './material-kit';
 
-import { ButtonWithInternalLink } from '.';
+import { PostCard, MKBox, Button } from '.';
 
-import colors from '../assets/oldTheme/colors.json';
 import { useMobileViewStatus } from '../hooks';
 
 type PostContainerProps = {
@@ -82,13 +82,15 @@ export const PostContainer = ({
 
       {pageId && postContents.length > maxNumberOfPosts && (
         <Flex justifyContent="center" mt="30px" mb="30px" fontSize={[2, 3]}>
-          <ButtonWithInternalLink
-            to={`/${pageId}`}
-            color={colors.background}
-            backgroundColor={colors.secondary}
-          >
-            See all
-          </ButtonWithInternalLink>
+          <Button
+            content={{
+              type: 'internal',
+              url: `/${pageId}`,
+              name: 'See all',
+            }}
+            color="quaternary"
+            backgroundColor="secondary"
+          />
         </Flex>
       )}
     </Container>
