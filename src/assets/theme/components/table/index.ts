bc011796252f6b16bad66a623b612907e6f604d7
Souchet Céline <celine.souchet@bonitasoft.com>
2023-08-11 17:40:16 +0200

    add missing table configuration

--- src/assets/theme/components/table/index.ts
+++ src/assets/theme/components/table/index.ts
@@ -14,19 +14,6 @@
  * limitations under the License.
  */
 
-export { MuiIconButton } from './muiIconButton';
-export { MuiTooltip } from './muiTooltip';
-export { MuiAppBar } from './muiAppBar';
-export { MuiContainer } from './muiContainer';
-export { MuiIcon } from './muiIcon';
-export { MuiSvgIcon } from './muiSvgIcon';
-export { MuiLink } from './muiLink';
-export { MuiButton } from './muiButton';
-
-export * from './card';
-export * from './dialog';
-export * from './form';
-export * from './list';
-export * from './menu';
-export * from './stepper';
-export * from './table';
+export { MuiTableCell } from './muiTableCell';
+export { MuiTableContainer } from './muiTableContainer';
+export { MuiTableHead } from './muiTableHead';
